68fa518c23a148c110baef5f9d6d0e1fabc0d055
Odaga <fabio.odaguiri@fatec.sp.gov.br>
2021-10-26 00:11:19 -0300

    Update MergeSort.java

--- merge/MergeSort.java
+++ merge/MergeSort.java
@@ -15,44 +15,20 @@ public class MergeSort {
     }
 
     private static void sort(int[] arr, int start, int end) {
-        Utils.printArray(arr);
-        System.out.println("Start:"+start);
-        System.out.println("End:"+end);
-
-        if (end - start > 1) {             System.out.println("If End ("+end + ")- Start ("+start+") >1, then:");
-
-            int middle = (start + end) / 2;             System.out.println("Middle:"+middle+"="+start+"-"+end+"/2");
-
-                                        System.out.println("Sort LEFT-HAND-SIDE SIDE OF THE ARRAY!");
+        if (end - start > 1) {
+            int middle = (start + end) / 2;
             sort(arr, start, middle);
-            
-                                        System.out.println("Sort right-hand-side OF THE ARRAY!");
             sort(arr, middle, end);
-
-                                        System.out.println("merge both sides OF THE ARRAY!");
             merge(arr, start, middle, end);
         }
     }
 
-    private static void merge(int[] arr, int start, int middle, int end) {         System.out.println("Start="+start+"; Middle="+middle+"; End="+end);
-        
-        int[] leftSide = new int[middle - start]; System.out.println("leftSide=");
-        
-        Utils.printArray(leftSide); System.out.println("= Middle="+middle+"- End="+end);
+    private static void merge(int[] arr, int start, int middle, int end) {
+        int[] leftSide = new int[middle - start];
         int[] rightSide = new int[end - middle];
-        System.out.println("rightSide=");
-        //System.out.println(Arrays.toString(rightSide));
-        Utils.printArray(rightSide);
-        System.out.println("= End="+end+"- Middle="+middle);
 
-        //System.out.println("For i = 0; i < "+leftSide.lenght+"; i++");
         for (int i = 0; i < leftSide.length; i++) {
-            System.out.println("i = "+i);
-            System.out.println("leftSide.length "+leftSide.length);
-            
             leftSide[i] = arr[start + i];
-            
-            System.out.println("i = "+i);
         }
 
         for (int i = 0; i < rightSide.length; i++) {
@@ -63,43 +39,20 @@ public class MergeSort {
         int topRight = 0;
 
         for (int i = start; i < end ; i++) {
-            System.out.println("i:"+i);
-                System.out.println("Array[i]:"+arr[i]+", Length:"+leftSide.length+
-                                    ", leftSide:"+leftSide+
-                                    ", topLeft:"+topLeft);
-
             if (topLeft >= leftSide.length) {
                 arr[i] = rightSide[topRight];
                 topRight++;
-                System.out.println("i:"+i+", Array[i]: "+arr[i]+", Length:"+rightSide.length+
-                ", leftSide:"+leftSide+
-                ", topLeft:"+topLeft);
-        
             }
             else if (topRight >= rightSide.length) {
                 arr[i] = leftSide[topLeft];
                 topLeft++;
-                System.out.println("i:"+i);
-                System.out.println("Array[i]:"+arr[i]+", Length:"+leftSide.length+
-                                    ", leftSide:"+leftSide+
-                                    ", topLeft:"+topLeft);
             }
             else if (leftSide[topLeft] < rightSide[topRight]) {
                 arr[i] = leftSide[topLeft];
                 topLeft++;
-                System.out.println("i:"+i);
-                System.out.println("Array[i]:"+arr[i]);
-                System.out.println(", Length:"+leftSide.length);
-                System.out.println(", leftSide:"+leftSide);
-                System.out.println(", topLeft:"+topLeft);
             } else  {
                 arr[i] = rightSide[topRight];
                 topRight++;
-                System.out.println("i:"+i);
-                System.out.println("Array[i]:"+arr[i]);
-                System.out.println(", Length:"+leftSide.length);
-                System.out.println(", leftSide:"+leftSide);
-                System.out.println(", topLeft:"+topLeft);
             }
         }
 
